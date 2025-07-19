36fe6bf9e24a716bd2cee8b1283441d5c8775b4e
Carava Jojo <102180250+InakiVA@users.noreply.github.com>
2025-07-18 19:53:33 -0600

    visualización casi completa de home study, falta incluir cómo agarra los grupos y sus progresos, al igual que cuadro de elegidas

--- study/views.py
+++ study/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
+from tags.models import Etiqueta
+from groups.models import Grupo
 
 
 class HomeView(TemplateView):
@@ -9,7 +11,10 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["href_estudio"] = reverse_lazy("estudio")
-
+        context["preguntas"] = ("Original", "Traducción", "Cualquiera")
+        context["orden"] = ("Nombre", "Progreso")
+        context["tags"] = Etiqueta.objects.all().order_by("etiqueta")
+        context["grupos"] = Grupo.objects.all().order_by("grupo")
         return context
 
     def is_mobile(request):
